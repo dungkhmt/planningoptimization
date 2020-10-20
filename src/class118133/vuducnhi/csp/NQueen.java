1bf504e77080892cc405f9e3e4214ebe4b12b09a
nomisrevol <vdn1999bxvp@gmail.com>
2020-10-20 07:19:33 +0700

    Rename file and update NQueen

--- src/class118133/vuducnhi/csp/NQueen.java
+++ src/class118133/vuducnhi/csp/NQueen.java
@@ -12,18 +12,18 @@ public class NQueen {
         System.out.print("N = ? ");
         int N = sc.nextInt();
         Model model = new Model(N + "-queens problem");
-        IntVar[] vars = new IntVar[N];
-        for(int q = 0; q < N; q++){
-            vars[q] = model.intVar("Q_"+q, 1, N);
+        IntVar[] X = new IntVar[N];
+        for (int q = 0; q < N; q++) {
+            X[q] = model.intVar("Q_"+q, 1, N);
         }
-        for(int i  = 0; i < N - 1; i++){
-            for(int j = i + 1; j < N; j++){
-                model.arithm(vars[i], "!=",vars[j]).post();
-                model.arithm(vars[i], "!=", vars[j], "-", j - i).post();
-                model.arithm(vars[i], "!=", vars[j], "+", j - i).post();
+        for (int i  = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                model.arithm(X[i], "!=", X[j]).post();
+                model.arithm(X[i], "!=", X[j], "-", j - i).post();
+                model.arithm(X[i], "!=", X[j], "+", j - i).post();
             }
         }
-
+        model.allDifferent(X).post();
         Solution solution = model.getSolver().findSolution();
         if (solution != null) {
             System.out.println(solution.toString());
